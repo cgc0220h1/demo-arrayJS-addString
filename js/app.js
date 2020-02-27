028c2080d6dc7f475d1e1d08447559e25e97df09
vanduc2514 <vanduc2514@gmail.com>
2020-02-27 09:53:11 +0700

    Sửa lại thuật toán

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
 let myColor = ["Red", "Green", "White", "Black"];
 function addString() {
-    let data = "";
-    for (let i = 0; i < myColor.length; i++) {
-        data += myColor[i] + ", ";
-    }
-    document.getElementById('result').innerHTML = data;
+    document.getElementById('result').innerHTML = myColor.join(",");
 }
